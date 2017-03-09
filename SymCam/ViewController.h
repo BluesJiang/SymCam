9f21f759b7f474467b28682a785b197ad28df9e7
BluesJiang <763400095@qq.com>
2017-03-09 14:42:23 +0800

    Project init

---
+++ SymCam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SymCam
+//
+//  Created by BluesJiang on 2017/3/9.
+//  Copyright © 2017年 blues. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SymCam/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SymCam
+//
+//  Created by BluesJiang on 2017/3/9.
+//  Copyright © 2017年 blues. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
